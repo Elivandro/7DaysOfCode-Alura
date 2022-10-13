f73d5b9b64f32744449797be532e1310e5154243
Elivandro <Elivandro@hotmail.comm.br>
2022-10-13 17:47:51 -0400

    switch route created

--- scuba-php/routes.php
+++ scuba-php/routes.php
@@ -1,4 +1,20 @@
 <?php
-$page = ($_GET['page']??'login').'.view';
-$content = file_get_contents(VIEW_FOLDER.$page);
-echo $content;
+$page = $_GET['page']??'login';
+
+switch($page){
+    case 'login':
+        do_login();
+        break;
+    case 'register':
+        do_register();
+        break;
+    case 'change-password':
+        do_change_password();
+        break;
+    case 'forget-password':
+        do_forget_password();
+        break;
+    default:
+        do_not_found();
+        break;
+}
